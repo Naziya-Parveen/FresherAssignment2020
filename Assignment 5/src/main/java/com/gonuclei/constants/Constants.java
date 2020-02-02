0f7dac10df1669bcb06cfe57fdd397f6eb29e8e5
Naziya Parveen <45765721+Naziya-Parveen@users.noreply.github.com>
2020-02-02 23:22:42 +0530

    Add files via upload

---
+++ Assignment 5/src/main/java/com/gonuclei/constants/Constants.java
@@ -0,0 +1,53 @@
+package com.gonuclei.constants;
+
+/**
+ * The interface Constants.
+ */
+public final  class Constants {
+
+  /**
+   * The constant AUTHORIZATION.
+   */
+  public final static String AUTHORIZATION = "Authorization";
+
+  /**
+   * The constant TOKEN_VALIDITY.
+   */
+  public final static int TOKEN_VALIDITY = 1000 * 60 * 60;
+
+  /**
+   * The constant EMAIL_SUBJECT.
+   */
+  public final static String EMAIL_SUBJECT = "Update";
+
+  /**
+   * The constant TOPIC_PATTERN.
+   */
+  public final static String TOPIC_PATTERN = "topic_";
+
+  /**
+   * The constant TOPIC_PATTERN_MATCH.
+   */
+  public final static String TOPIC_PATTERN_MATCH = "topic_.*";
+
+  /**
+   * The constant KAFKA_CONTAINER_FACTORY.
+   */
+  public final static String KAFKA_CONTAINER_FACTORY = "applicaionKafkaContainerFactory";
+
+  /**
+   * The constant KAFKA_CONTEXT_HOLDER.
+   */
+  public final static String KAFKA_CONTEXT_HOLDER = "applicaionKafkaContextHolder";
+
+  /**
+   * The constant SUBSTRING_TOKEN.
+   */
+  public final static int SUBSTRING_TOKEN = 7;
+
+  /**
+   * The constant AUTHORIZATION_TYPE.
+   */
+  public final static String AUTHORIZATION_TYPE = "Bearer ";
+
+}
